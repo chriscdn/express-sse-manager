6cca1cd3e75766a3884a9c1b53220535fb5ee6e7
Christopher Meyer <chris@schwiiz.org>
2021-05-17 17:30:25 +0200

    flush socket option

--- express/routes.js
+++ express/routes.js
@@ -1,55 +1,88 @@
 const express = require('express')
 const router = express.Router()
-
-// const middleware = require('./middleware')
 const manager = require('./manager')
+const debounce = require('lodash.debounce')
 
-const middleware = (req, res, next) => {
+function randomString(length) {
+	let result = ''
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+	const charactersLength = characters.length
 
-	res.writeHead(200, {
-		'Content-Type': 'text/event-stream',
-		'Cache-Control': 'no-cache',
-		'Connection': 'keep-alive',
-		'Access-Control-Allow-Origin': '*'
-	})
-
-	const keepAliveHandshake = setInterval(() => {
-		res.write(':keepalive')
-	}, 15000)
-
-	res.on('finish', () => clearInterval(keepAliveHandshake))
-	res.on('close', () => clearInterval(keepAliveHandshake))
-
-	/**
-	 * Add function to response which allow to send events to the client
-	 * @param evt
-	 * @param json
-	 * @param [id]
-	 */
-	res.sse = (evt, json, id) => {
-		res.write('\n')
-
-		if (id) {
-			res.write(`id: ${id}\n`)
-		}
-
-		res.write(`retry: 3000\n`)
-		res.write(`event: ${evt}\n`)
-		res.write(`data: ${JSON.stringify(json)}\n\n`)
-
-		// jury is out on this... required with compression middleware? 
-		// some error about using flushHeaders instead.
-		// if (res.flush) {
-		// 	res.flush()
-		// }
+	for (let i = 0; i < length; i++) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength))
 	}
 
-	next()
+	return result
 }
 
-router.get('/:key', middleware, (req, res) => {
-	const key = req.params.key
-	manager.registerConnection(key, req, res)
-})
+function routerGenerator(size, debounceTime) {
 
-module.exports = router
+	const bigRandomString = randomString(size)
+
+	const middleware = (req, res, next) => {
+		res.writeHead(200, {
+			'Content-Type': 'text/event-stream',
+			'Cache-Control': 'no-cache',
+			'Connection': 'keep-alive',
+			'Access-Control-Allow-Origin': '*'
+		})
+
+		const keepAliveHandshake = setInterval(() => {
+			res.write(':keepalive\n')
+		}, 15000)
+
+		res.on('finish', () => clearInterval(keepAliveHandshake))
+		res.on('close', () => clearInterval(keepAliveHandshake))
+
+		res._flushed = debounce(function() {
+			res.write(`:${bigRandomString}\n`)
+		}, debounceTime)
+
+		/**
+		 * Add function to response which allow to send events to the client
+		 * @param evt
+		 * @param json
+		 * @param [id]
+		 */
+		res.sse = (evt, json, id) => {
+			res.write('\n')
+
+			if (id) {
+				res.write(`id: ${id}\n`)
+			}
+
+			res.write(`retry: 3000\n`)
+			res.write(`event: ${evt}\n`)
+			res.write(`data: ${JSON.stringify(json)}\n\n`)
+
+			// jury is out on this... required with compression middleware? 
+			// some error about using flushHeaders instead.
+			// if (res.flush) {
+			// 	res.flush()
+			// }
+			// 
+				
+			// Proxies may buffer the response, which prevents
+			// messages from being sent.  These routes can be configured with
+			// a `size` and `debounceTime` parameter to push a comment to effectively
+			// flush the cache.
+			if (size > 0) {
+				res._flushed()
+			}		
+		}
+
+		next()
+	}
+
+	router.get('/:key', middleware, (req, res) => {
+		const key = req.params.key
+		manager.registerConnection(key, req, res)
+	})
+
+	return router
+
+}
+
+module.exports = (size = 0, debounceTime = 750) => {
+	return routerGenerator(size, debounceTime)
+}
